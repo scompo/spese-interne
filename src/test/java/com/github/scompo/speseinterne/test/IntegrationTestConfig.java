70e625150cf1033fc42f4a7ed74931fe03f4414d
Mauro Scomparin <scompo@gmail.com>
2015-09-18 01:19:37 +0200

    Added transactional configuration

--- src/test/java/com/github/scompo/speseinterne/test/IntegrationTestConfig.java
+++ src/test/java/com/github/scompo/speseinterne/test/IntegrationTestConfig.java
@@ -5,12 +5,14 @@ import java.lang.annotation.RetentionPolicy;
 
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.boot.test.WebIntegrationTest;
+import org.springframework.test.context.transaction.TransactionConfiguration;
 
 import com.github.scompo.speseinterne.Application;
 
 @Retention(RetentionPolicy.RUNTIME)
-@SpringApplicationConfiguration(classes = {Application.class})
+@SpringApplicationConfiguration(classes = { Application.class })
 @WebIntegrationTest
+@TransactionConfiguration(defaultRollback = true)
 public @interface IntegrationTestConfig {
 
 }
