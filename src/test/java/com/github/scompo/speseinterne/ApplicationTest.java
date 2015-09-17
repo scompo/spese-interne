97c8032818ddb1cc86031f9afd71096eb7b9699d
Mauro Scomparin <scompo@gmail.com>
2015-09-17 21:53:44 +0200

    now it runs..

--- src/test/java/com/github/scompo/speseinterne/ApplicationTest.java
+++ src/test/java/com/github/scompo/speseinterne/ApplicationTest.java
@@ -1,16 +1,18 @@
 package com.github.scompo.speseinterne;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.WebIntegrationTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = Application.class)
+@WebIntegrationTest
 public class ApplicationTest {
 
 	@Before
